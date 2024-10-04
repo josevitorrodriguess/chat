8ff89c7d65218f807e580d0a0793fb803aed3bc1
Jose <josevitorrodrigues17@gmail.com>
2024-10-04 12:55:42 -0300

    feat(models): config gorm on user struct to migrations

--- internal/api/user/models/user.go
+++ internal/api/user/models/user.go
@@ -1,8 +1,8 @@
 package models
 
 type User struct {
-	ID       string `json:"id"`       //MAKES UUID
-	Username string `json:"username"` //UNIQUE
-	Email    string `json:"email"`    //UNIQUE
-	Password string `json:"password"` //SHA256
+	ID       string `gorm:"type:uuid;default:gen_random_uuid();primaryKey"` // UUID como chave primária
+	Username string `gorm:"unique;not null"`                                 // Nome de usuário único e não nulo
+	Email    string `gorm:"unique;not null"`                                 // E-mail único e não nulo
+	Password string `gorm:"not null"`                                        // Senha não nula
 }
