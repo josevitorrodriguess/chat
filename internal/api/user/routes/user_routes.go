2797b296650a72880b35e4eddc11756fe4777ad2
Jose <josevitorrodrigues17@gmail.com>
2024-10-05 00:28:04 -0300

    feat(routes): routes for user

---
+++ internal/api/user/routes/user_routes.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/josevitorrodriguess/chat/internal/api/user/controller"
+	"github.com/josevitorrodriguess/chat/internal/api/user/repository"
+	"github.com/josevitorrodriguess/chat/internal/api/user/service"
+	"gorm.io/gorm"
+)
+
+func UserRoutes(r *gin.RouterGroup, db *gorm.DB) {
+	userRepo := repository.NewUserRepository(db)
+	userService := service.NewUserService(userRepo)
+	userController := controller.NewUserController(userService)
+
+	r.POST("/signUp", userController.CreateUser)
+}
