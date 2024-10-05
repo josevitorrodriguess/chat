7da942df8d80639af0c7c52a087fb836d86fb920
Jose <josevitorrodrigues17@gmail.com>
2024-10-05 00:26:22 -0300

    fix(models): add validation for password and name

--- internal/api/user/models/user.go
+++ internal/api/user/models/user.go
@@ -1,8 +1,22 @@
 package models
 
+import (
+	"github.com/go-playground/validator/v10"
+)
+
 type User struct {
-	ID       string `gorm:"type:uuid;default:gen_random_uuid();primaryKey"` // UUID como chave primária
-	Username string `gorm:"unique;not null"`                                 // Nome de usuário único e não nulo
-	Email    string `gorm:"unique;not null"`                                 // E-mail único e não nulo
-	Password string `gorm:"not null"`                                        // Senha não nula
+	ID       string `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	Username string `gorm:"unique;not null" validate:"required,min=6"` // Validação: requer ao menos 1 caractere
+	Email    string `gorm:"unique;not null"`
+	Password string `gorm:"not null" validate:"required,min=6`
+}
+
+var validate *validator.Validate
+
+func init() {
+	validate = validator.New()
+}
+
+func (u *User) Validate() error {
+	return validate.Struct(u)
 }
