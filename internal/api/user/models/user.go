bedc2060cfaf4a97a3ac9de731421e97f22cc1c5
Jose <josevitorrodrigues17@gmail.com>
2024-10-06 11:40:11 -0300

    fix: change string to uuid

--- internal/api/user/models/user.go
+++ internal/api/user/models/user.go
@@ -2,13 +2,14 @@ package models
 
 import (
 	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
 )
 
 type User struct {
-	ID       string `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
-	Username string `gorm:"unique;not null" validate:"required,min=6"` // Validação: requer ao menos 1 caractere
-	Email    string `gorm:"unique;not null"`
-	Password string `gorm:"not null" validate:"required,min=6`
+	ID       uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	Username string    `gorm:"unique;not null" validate:"required,min=6"` // Validação: requer ao menos 1 caractere
+	Email    string    `gorm:"unique;not null"`
+	Password string    `gorm:"not null" validate:"required,min=6`
 }
 
 var validate *validator.Validate
