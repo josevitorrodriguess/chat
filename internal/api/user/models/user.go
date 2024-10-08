ea0219b3b41c61035488d58fc7ccf189d269c97e
Jose <josevitorrodrigues17@gmail.com>
2024-10-08 09:32:31 -0300

    feat: add validation for struct

--- internal/api/user/models/user.go
+++ internal/api/user/models/user.go
@@ -7,9 +7,9 @@ import (
 
 type User struct {
 	ID       uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
-	Username string    `gorm:"unique;not null" validate:"required,min=6"` // Validação: requer ao menos 1 caractere
+	Username string    `gorm:"unique;not null" validate:"required,min=6"` 
 	Email    string    `gorm:"unique;not null"`
-	Password string    `gorm:"not null" validate:"required,min=6`
+	Password string    `gorm:"not null" validate:"required,min=6"`
 }
 
 var validate *validator.Validate
