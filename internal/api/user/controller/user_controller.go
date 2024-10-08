735bc5f0d828e2b9bf220091bbcecccbe9fd5cb9
Jose <josevitorrodrigues17@gmail.com>
2024-10-08 10:05:11 -0300

    feat: controller for route update user

--- internal/api/user/controller/user_controller.go
+++ internal/api/user/controller/user_controller.go
@@ -1,12 +1,14 @@
 package controller
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"github.com/josevitorrodriguess/chat/internal/api/user/models"
 	"github.com/josevitorrodriguess/chat/internal/api/user/service"
+	"gorm.io/gorm"
 )
 
 type userController struct {
@@ -49,7 +51,7 @@ func (uc *userController) FinAll(ctx *gin.Context) {
 func (uc *userController) FindById(ctx *gin.Context) {
 
 	idParam := ctx.Param("id")
-	
+
 	id, err := uuid.Parse(idParam)
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid UUID format"})
@@ -63,4 +65,33 @@ func (uc *userController) FindById(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, user)
+}
+
+func (uc *userController) Update(ctx *gin.Context) {
+	var user models.User
+
+	idParam := ctx.Param("id")
+	id, err := uuid.Parse(idParam)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid UUID format"})
+		return
+	}
+
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	updatedUser, err := uc.serv.Update(id, user)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update user"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, updatedUser)
 }
